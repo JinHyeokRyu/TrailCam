4e944769bab8d1b33d4b47c84a6197dc0a79a03b
RyuJinHyeok <101165696+RyuJinHyeok@users.noreply.github.com>
2026-03-13 15:23:20 +0900

    code upload

---
+++ hw1.py
@@ -0,0 +1,60 @@
+import cv2 as cv
+import numpy as np
+
+video = cv.VideoCapture(0)
+
+if video.isOpened():
+    # frame settings
+    fps = 10
+    wait_msec = int(1 / fps * 1000)
+
+    # record settings
+    video_recorder = cv.VideoWriter()
+    fourcc = cv.VideoWriter_fourcc(*'XVID')
+    record_path = './data/new_video.avi'
+    recording = False
+    
+    # alpha blending settings
+    blend_mode = False
+    prev_img = None
+    alpha = 0.5
+
+    while True:
+        valid, img = video.read()
+        if not valid:
+            break
+        
+        # blend
+        if blend_mode and prev_img is not None:
+            img = (alpha * prev_img + (1 - alpha) * img).astype(np.uint8)
+        
+        prev_img = img.copy()
+
+        # record
+        if recording:
+            video_recorder.write(img)
+            cv.putText(img, 'Recording...', (10, 30), cv.FONT_HERSHEY_DUPLEX, 1, (0, 0, 255))
+
+        cv.imshow('Video Player', img)
+
+
+        key = cv.waitKey(wait_msec)
+        
+        if key == ord(' '):
+            recording = not recording
+
+            # start recording
+            if recording and not video_recorder.isOpened():
+                h, w, _ = img.shape
+                video_recorder.open(record_path, fourcc, fps, (w, h))
+
+            # save video file
+            elif not recording:
+                video_recorder.release()
+                print('recorded!')
+
+        elif key == ord('b'):
+            blend_mode = not blend_mode
+
+        elif key == 27:
+            break
